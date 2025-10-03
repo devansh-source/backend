cdc90c25c391ea64ab2a9a87abad4544ea5a3f3e
devanshpanchal123-pixel <devanshpanchal123@gmail.com>
2025-10-03 08:53:04 +0530

    Add user authentication feature

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+// In routes/authRoutes.js
+
+import express from "express";
+import { registerUser, loginUser } from "../controllers/authController.js";
+
+const router = express.Router();
+
+// Define the routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+export default router;
